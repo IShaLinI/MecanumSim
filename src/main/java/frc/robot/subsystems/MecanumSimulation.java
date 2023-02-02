746d40ec5920afd67ca9231763cbadc7a9197325
IShaLinI <mrsaltinecracker1@gmail.com>
2023-02-02 12:29:39 -0800

    More progress

--- src/main/java/frc/robot/subsystems/MecanumSimulation.java
+++ src/main/java/frc/robot/subsystems/MecanumSimulation.java
@@ -4,13 +4,15 @@
 
 package frc.robot.subsystems;
 
+import java.util.function.DoubleSupplier;
+
 import com.ctre.phoenix.motorcontrol.TalonFXSimCollection;
 import com.ctre.phoenix.sensors.BasePigeonSimCollection;
 
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
-import frc.robot.Constants;
 
 public class MecanumSimulation {
 
@@ -19,66 +21,98 @@ public class MecanumSimulation {
     private TalonFXSimCollection mBackLeftSimCollection;
     private TalonFXSimCollection mBackRightSimCollection;
 
+    private DoubleSupplier mFrontLeftMotorSet;
+    private DoubleSupplier mFrontRightMotorSet;
+    private DoubleSupplier mBackLeftMotorSet;
+    private DoubleSupplier mBackRightMotorSet;
+
     private BasePigeonSimCollection mBasePigeonSimCollection;
 
     private DCMotor mMotor;
     private double mGearRatio;
+    private double mWheelDiameterMeters;
+    private double kDistancePerPulse;
 
     private FlywheelSim mFrontLeftWheelSimulation;
     private FlywheelSim mFrontRightWheelSimulation;
     private FlywheelSim mBackLeftWheelSimulation;
     private FlywheelSim mBackRightWheelSimulation;
 
-    public MecanumSimulation(TalonFXSimCollection[] _motorSims, BasePigeonSimCollection _pigeonSim, DCMotor _motor, double _gearRatio) {
+    private double dtSeconds;
+
+    public MecanumSimulation(
+        TalonFXSimCollection[] _motorSims, 
+        DoubleSupplier[] _motorSets,
+        BasePigeonSimCollection _pigeonSim,
+        DCMotor _motor, 
+        double _gearRatio, 
+        double _wheelDiameterMeters,
+        double _characterizationCoefficients[],
+        double _dtSeconds
+) {
 
         mFrontLeftSimCollection = _motorSims[0];
         mFrontRightSimCollection = _motorSims[1];
         mBackLeftSimCollection = _motorSims[2];
         mBackRightSimCollection = _motorSims[3];
+
         mBasePigeonSimCollection = _pigeonSim;
+
         mMotor = _motor;
         mGearRatio = _gearRatio;
+        mWheelDiameterMeters = _wheelDiameterMeters;
+        kDistancePerPulse = (mWheelDiameterMeters * Math.PI) / (2048.0 * mGearRatio);
 
         mFrontLeftWheelSimulation = new FlywheelSim(
-            LinearSystemId.identifyVelocitySystem(
-                Constants.DRIVETRAIN_kV,
-                Constants.DRIVETRAIN_kA
-            ),
-            mMotor,
-            mGearRatio
-        );
+                LinearSystemId.identifyVelocitySystem(
+                        _characterizationCoefficients[1],
+                        _characterizationCoefficients[2]),
+                mMotor,
+                mGearRatio);
 
         mFrontRightWheelSimulation = new FlywheelSim(
-            LinearSystemId.identifyVelocitySystem(
-                Constants.DRIVETRAIN_kV,
-                Constants.DRIVETRAIN_kA
-            ),
-            mMotor,
-            mGearRatio
-        );
+                LinearSystemId.identifyVelocitySystem(
+                        _characterizationCoefficients[1],
+                        _characterizationCoefficients[2]),
+                mMotor,
+                mGearRatio);
 
         mBackLeftWheelSimulation = new FlywheelSim(
-            LinearSystemId.identifyVelocitySystem(
-                Constants.DRIVETRAIN_kV,
-                Constants.DRIVETRAIN_kA
-            ),
-            mMotor,
-            mGearRatio
-        );
+                LinearSystemId.identifyVelocitySystem(
+                        _characterizationCoefficients[1],
+                        _characterizationCoefficients[2]),
+                mMotor,
+                mGearRatio);
 
         mBackRightWheelSimulation = new FlywheelSim(
-            LinearSystemId.identifyVelocitySystem(
-                Constants.DRIVETRAIN_kV,
-                Constants.DRIVETRAIN_kA
-            ),
-            mMotor,
-            mGearRatio
-        );
+                LinearSystemId.identifyVelocitySystem(
+                        _characterizationCoefficients[1],
+                        _characterizationCoefficients[2]),
+                mMotor,
+                mGearRatio);
 
+        mFrontLeftMotorSet = _motorSets[0];
+        mFrontRightMotorSet = _motorSets[1];
+        mBackLeftMotorSet = _motorSets[2];
+        mBackRightMotorSet = _motorSets[3];
+
+        dtSeconds = _dtSeconds;
     }
 
-    public void update(){
-        
+    public void update() {
+        mFrontLeftWheelSimulation.setInput(mFrontLeftMotorSet.getAsDouble() * RobotController.getBatteryVoltage());
+        mFrontRightWheelSimulation.setInput(mFrontRightMotorSet.getAsDouble() * RobotController.getBatteryVoltage());
+        mBackLeftWheelSimulation.setInput(mBackLeftMotorSet.getAsDouble() * RobotController.getBatteryVoltage());
+        mBackRightWheelSimulation.setInput(mBackRightMotorSet.getAsDouble() * RobotController.getBatteryVoltage());
+
+        mFrontLeftWheelSimulation.update(dtSeconds);
+        mFrontRightWheelSimulation.update(dtSeconds);
+        mBackLeftWheelSimulation.update(dtSeconds);
+        mBackRightWheelSimulation.update(dtSeconds);
+
+        mFrontLeftSimCollection.setIntegratedSensorRawPosition(mFrontLeftWheelSimulation.)
+
+
     }
 
 }
